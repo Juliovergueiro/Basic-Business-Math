c4986ed32b65f8117386d61dc1cfd2351d4e5ba8
Julio Vergueiro <89145136+Fyonsly@users.noreply.github.com>
2022-01-12 10:40:22 -0300

    Add files via upload

---
+++ basicMath.js
@@ -0,0 +1,37 @@
+let ex1 = 6 + 10 * 2 // A multiplicação vem antes da soma, regra
+console.log(ex1)
+let ex2 = 11 - 11 - Math.pow(2, 2) // Forma de fazer ao quadrado; Math.pow(numero, expoente) ou seja Math.pow(9, 2) = 9²
+console.log(ex2)
+let ex3 = Math.pow((11 - 2), 2)
+console.log(ex3)
+let ex4 = 41 - (13 + 8) // 20
+console.log(ex4)
+let ex5 = (8 + 4) * 6 // 72
+console.log(ex5)
+let ex6 = 9 + 15 / Math.sqrt(25) // Math.sqrt é o comando pra trazer a raiz quadrada de um número
+// 15 / 5 = 3, a divisão vem primeiro, então é feito 9 + 3 = 12
+console.log(ex6)
+let ex7 = 55 + 11 * 5 // 55 + 55 = 110, a multiplicação 11 * 5 = 55 vem antes da soma
+console.log(ex7)
+let ex8 = Math.pow(2, 3) - Math.pow(2, 3) // Forma de fazer ao cubo (numero, expoente³)
+console.log(ex8)
+let ex9
+console.log(ex9)
+let ex10 = Math.sqrt((4937 / 3) - (4.7 * 23))
+console.log(ex10)
+let ex11 = Math.pow((7 - 5), 2)
+console.log(ex11)
+let ex12 = Math.sqrt(121) + (3 * (8 - 3)) // Escrito; sqrt121 + 3(8 - 3) ; ou seja, resolve o 8 - 3 (5) e multiplica o 3 por 5 (quando os parenteses estão colados, é por quê é pra multiplicar pelo resultado. No JS não existe essa conversão, então apenas adicione um *)
+console.log(ex12)
+let ex13 = 36 / ((2 * 2) + 5) // 36 / 9
+console.log(ex13)
+let ex14 = 80 - (20 / 4) + 6 // 80 - 5 + 6 = 81
+console.log(ex14)
+let ex15 = (51 - 48) * 3 + (7 * 2)
+console.log(ex15)
+let ex16 = (Math.sqrt(36) + 11) - (31 - 16)
+console.log(ex16)
+let ex17 = 80 / Math.pow(Math.pow(9, 2) - 7 * 11, 2)
+console.log(ex17)
+let ex18 = 108 / Math.pow(Math.pow(3, 3) - 6 * 4, 2)
+console.log(ex18)
